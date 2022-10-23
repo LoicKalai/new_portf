1a0a792cd9471a60689717526978628505a35ce4
LoicKalai <loic.kalaidjoglou@icloud.com>
2022-10-23 14:27:41 +0200

    mise en place menu burger

---
+++ js/burger.js
@@ -0,0 +1,11 @@
+// let btnmenu = document.getElementById("btnburger");
+// let sidebar = document.getElementById("sidebar");
+
+
+// // Fonction : ouvre / ferme le volet
+// function ouvreFermeVolet(){
+//     sidebar.classList.toggle("ouvert");
+//   }
+  
+//   // Activation et programmation des clics
+//   btnmenu.addEventListener('click', ouvreFermeVolet);
